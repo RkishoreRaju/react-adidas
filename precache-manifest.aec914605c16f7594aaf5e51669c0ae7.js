7f2d967d418e54373da0f58926e135fd1c46cd84
RkishoreRaju <rudrarajukishore1@gmail.com>
2020-04-19 17:17:41 +0530

    Updates

---
+++ precache-manifest.aec914605c16f7594aaf5e51669c0ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ec4b2f42a31fdb3435da905cbdd8fd",
+    "url": "/react-adidas/index.html"
+  },
+  {
+    "revision": "e761facacd3ac1f36df2",
+    "url": "/react-adidas/static/css/main.9491842b.chunk.css"
+  },
+  {
+    "revision": "d123d036a33193c81f8d",
+    "url": "/react-adidas/static/js/2.902208e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-adidas/static/js/2.902208e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e761facacd3ac1f36df2",
+    "url": "/react-adidas/static/js/main.30010813.chunk.js"
+  },
+  {
+    "revision": "e2e63d4378a41ee22c34",
+    "url": "/react-adidas/static/js/runtime-main.2df944ab.js"
+  }
+]);
